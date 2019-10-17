98d7b5668484ce9d09fae4c71dbd570403359332
Keita Miyashita <koma@Koma-MacBook-Pro.local>
2019-10-17 12:11:21 +0900

    シリアル通信を

--- main.py
+++ main.py
@@ -7,14 +7,33 @@ import getch
 # PIN_NUM = [2, 3, 4, 17, 27, 22, 10]
 PIN_NUM = [2, 3, 4]
 
+DEL_TIME = 0.1
 
 def setupGPIO():
     GPIO.setmode(GPIO.BCM)
     for n in PIN_NUM:
         GPIO.setup(n, GPIO.OUT)
 
+def bang(pin):
+    GPIO.output(pin, 1)
+    time.sleep(0.01)
+    GPIO.output(pin, 0)
 
-def interpretation(key):
+def serial(key):
+    print('{}: {:07b}'.format(key.encode('utf-8'), ord(key)))
+    b = int(ord(key))
+
+    # start bang
+    bang(PIN_NUM[0])
+    time.sleep(DEL_TIME)
+
+    # send 1 byte
+    for i in range(8):
+        if (b >> i) & 0b1:
+            bang(PIN_NUM[0])
+        time.sleep(DEL_TIME)            
+
+def parallel(key):
     # 8bit以下だけ通すようにしたい
     print('{}: {:07b}'.format(key.encode('utf-8'), ord(key)))
     b = int(ord(key))
@@ -33,19 +52,19 @@ def main():
             print('please enter characters')
             keys = input()
             for key in keys:
-                interpretation(key)
+                parallel(key)
                 time.sleep(0.1)
 
 def main2():
     # 押したキーにすぐ反応ver
         key = getch.getch()
-        interpretation(key)
+        serial(key)
 
 if __name__ == '__main__':
     setupGPIO()
     try:
         while True:
-            main()
+            main2()
     except KeyboardInterrupt:
         GPIO.cleanup()
         print('end')
