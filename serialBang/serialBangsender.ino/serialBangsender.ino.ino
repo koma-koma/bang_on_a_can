7538b59b27742b30e04a312232820a52bf14e483
Keita Miyashita <koma@Koma-MacBook-Pro.local>
2019-12-07 16:45:23 +0900

    2回検知すると戻っちゃうやつを修正

--- serialBang/serialBangsender.ino/serialBangsender.ino.ino
+++ serialBang/serialBangsender.ino/serialBangsender.ino.ino
@@ -26,9 +26,6 @@ void sendBang(int sendByte) {
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
   // initialize the LED pin as an output:
   pinMode(bangpin, OUTPUT);
 }
