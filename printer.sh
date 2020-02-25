84f11d266c17df0d42764dc0b71c2615d3e180be
koma <keikei12@gmail.com>
2020-02-25 17:12:23 +0900

    自動起動用設定

---
+++ printer.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd /home/pi/Documents/myProjects/bang_on_a_can/pd/
+pd -nogui bang2int.pd printer.pd 12pbs.pd
+		 
---
+++ relay.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd /home/pi/Documents/myProjects/bang_on_a_can/pd/
+pd -nogui bang2int.pd relay.pd 10pbs.pd 5000msec.pd
